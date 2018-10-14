e8cd0727d42955bf51d4b757a2a9491fe19d9a4b
JustasMasiulis <justas.masiulis@yahoo.com>
2018-10-14 17:10:20 +0300

    some extra docs

--- include/io/async_file.hpp
+++ include/io/async_file.hpp
@@ -25,24 +25,34 @@ namespace ntw::io {
         using base_type = detail::base_file<Traits>;
 
     public:
-        NTW_INLINE basic_async_file()  = default;
-        NTW_INLINE ~basic_async_file() = default;
+        NTW_INLINE basic_async_file() = default;
 
         template<class ObjectHandle>
         NTW_INLINE basic_async_file(const ObjectHandle& handle)
             : base_type(unwrap_handle(handle))
         {}
 
+        /// \brief Writes data to file using NtWriteFile API.
+        /// \param data The data to write to file.
+        /// \param size The size of data to write to the file.
+        /// \param offset The offset from the beggining of file to write data to.
+        /// \param query The query object containing optional event, callback, etc.
         template<class QueryData>
         NT_FN write(const void*   data,
                     unsigned long size,
                     std::int64_t  offset,
                     QueryData&    query) const noexcept;
 
+        /// \brief Reads data from file using NtReadFile API.
+        /// \param data The buffer into which data will be read.
+        /// \param size The size of data to read from the file.
+        /// \param offset The offset from the beggining of file to read data from.
+        /// \param query The query object containing optional event, callback, etc.
         template<class QueryData>
-        NT_FN
-        read(void* buffer, unsigned long size, std::int64_t offset, QueryData& query) const
-            noexcept;
+        NT_FN read(void*         buffer,
+                   unsigned long size,
+                   std::int64_t  offset,
+                   QueryData&    query) const noexcept;
 
         template<class QueryData>
         NT_FN device_io_control(unsigned long control_code,
