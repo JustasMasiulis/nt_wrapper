24924a420495b7bcfc338c1453cbe54519940efb
JustasMasiulis <justas.masiulis@yahoo.com>
2019-02-27 22:28:05 +0200

    attribute tweaks

--- impl/obj/attributes.inl
+++ impl/obj/attributes.inl
@@ -3,96 +3,121 @@
 
 namespace ntw::obj {
 
-    NTW_INLINE attributes::attributes()
+    template<class D>
+    NTW_INLINE constexpr D& basic_attribute_options<D>::inherit()
     {
-        _attributes.Length                   = sizeof(_attributes);
-        _attributes.RootDirectory            = nullptr;
-        _attributes.ObjectName               = nullptr;
-        _attributes.Attributes               = 0;
-        _attributes.SecurityDescriptor       = nullptr;
-        _attributes.SecurityQualityOfService = nullptr;
+        static_cast<D*>(this)->options() |= OBJ_INHERIT;
+        return static_cast<D&>(*this);
     }
 
-    OBJECT_ATTRIBUTES& attributes::get() { return _attributes; }
+    template<class D>
+    NTW_INLINE constexpr D& basic_attribute_options<D>::permanent()
+    {
+        static_cast<D*>(this)->options() |= OBJ_PERMANENT;
+        return static_cast<D&>(*this);
+    }
 
-    const OBJECT_ATTRIBUTES& attributes::get() const { return _attributes; }
+    template<class D>
+    NTW_INLINE constexpr D& basic_attribute_options<D>::exclusive()
+    {
+        static_cast<D*>(this)->options() |= OBJ_EXCLUSIVE;
+        return static_cast<D&>(*this);
+    }
+
+    template<class D>
+    NTW_INLINE constexpr D& basic_attribute_options<D>::case_insensitive()
+    {
+        static_cast<D*>(this)->options() |= OBJ_CASE_INSENSITIVE;
+        return static_cast<D&>(*this);
+    }
+
+    template<class D>
+    NTW_INLINE constexpr D& basic_attribute_options<D>::open()
+    {
+        static_cast<D*>(this)->options() |= OBJ_OPENIF;
+        return static_cast<D&>(*this);
+    }
+
+    template<class D>
+    NTW_INLINE constexpr D& basic_attribute_options<D>::open_symlink()
+    {
+        static_cast<D*>(this)->options() |= OBJ_OPENLINK;
+        return static_cast<D&>(*this);
+    }
+
+    template<class D>
+    NTW_INLINE constexpr D& basic_attribute_options<D>::kernel_handle()
+    {
+        static_cast<D*>(this)->options() |= OBJ_KERNEL_HANDLE;
+        return static_cast<D&>(*this);
+    }
+
+    template<class D>
+    NTW_INLINE constexpr D& basic_attribute_options<D>::enforce_access_check()
+    {
+        static_cast<D*>(this)->options() |= OBJ_FORCE_ACCESS_CHECK;
+        return static_cast<D&>(*this);
+    }
+
+    template<class D>
+    NTW_INLINE constexpr D& basic_attribute_options<D>::ignore_impersonated_devicemap()
+    {
+        static_cast<D*>(this)->options() |= OBJ_IGNORE_IMPERSONATED_DEVICEMAP;
+        return static_cast<D&>(*this);
+    }
+
+    template<class D>
+    NTW_INLINE constexpr D& basic_attribute_options<D>::dont_reparse()
+    {
+        static_cast<D*>(this)->options() |= OBJ_DONT_REPARSE;
+        return static_cast<D&>(*this);
+    }
+
+
+    NTW_INLINE constexpr unsigned long& attribute_options::options() { return _options; }
+
+    NTW_INLINE constexpr unsigned long attribute_options::get() const { return _options; }
+
+
+    NTW_INLINE constexpr unsigned long& attributes::options()
+    {
+        return _attributes.Attributes;
+    }
+
+    NTW_INLINE constexpr attributes::attributes()
+        : _attributes{ sizeof(_attributes), nullptr, nullptr, 0, nullptr, nullptr }
+    {}
+
+    NTW_INLINE constexpr OBJECT_ATTRIBUTES& attributes::get() { return _attributes; }
+
+    NTW_INLINE constexpr const OBJECT_ATTRIBUTES& attributes::get() const
+    {
+        return _attributes;
+    }
 
     template<class Handle>
-    NTW_INLINE attributes& attributes::parent(const Handle& parent)
+    NTW_INLINE constexpr attributes& attributes::parent(const Handle& parent)
     {
         _attributes.RootDirectory = detail::unwrap_handle(parent);
         return *this;
     }
 
-    NTW_INLINE attributes& attributes::security_desc(void* descriptor)
+    NTW_INLINE constexpr attributes& attributes::security_desc(void* descriptor)
     {
         _attributes.SecurityDescriptor = descriptor;
         return *this;
     }
 
-    NTW_INLINE attributes& attributes::security_quality(void* quality_of_service)
+    NTW_INLINE constexpr attributes& attributes::security_quality(
+        void* quality_of_service)
     {
         _attributes.SecurityQualityOfService = quality_of_service;
         return *this;
     }
 
-    NTW_INLINE attributes& attributes::inherit()
+    NTW_INLINE constexpr attributes& attributes::options(attribute_options options)
     {
-        _attributes.Attributes |= OBJ_INHERIT;
-        return *this;
-    }
-
-    NTW_INLINE attributes& attributes::permanent()
-    {
-        _attributes.Attributes |= OBJ_PERMANENT;
-        return *this;
-    }
-
-    NTW_INLINE attributes& attributes::exclusive()
-    {
-        _attributes.Attributes |= OBJ_EXCLUSIVE;
-        return *this;
-    }
-
-    NTW_INLINE attributes& attributes::case_insensitive()
-    {
-        _attributes.Attributes |= OBJ_CASE_INSENSITIVE;
-        return *this;
-    }
-
-    NTW_INLINE attributes& attributes::open()
-    {
-        _attributes.Attributes |= OBJ_OPENIF;
-        return *this;
-    }
-
-    NTW_INLINE attributes& attributes::open_symlink()
-    {
-        _attributes.Attributes |= OBJ_OPENLINK;
-        return *this;
-    }
-
-    NTW_INLINE attributes& attributes::kernel_handle()
-    {
-        _attributes.Attributes |= OBJ_KERNEL_HANDLE;
-        return *this;
-    }
-
-    NTW_INLINE attributes& attributes::enforce_access_check()
-    {
-        _attributes.Attributes |= OBJ_FORCE_ACCESS_CHECK;
-        return *this;
-    }
-
-    NTW_INLINE attributes& attributes::ignore_impersonated_devicemap()
-    {
-        _attributes.Attributes |= OBJ_IGNORE_IMPERSONATED_DEVICEMAP;
-        return *this;
-    }
-
-    NTW_INLINE attributes& attributes::dont_reparse()
-    {
-        _attributes.Attributes |= OBJ_DONT_REPARSE;
+        _attributes.Attributes = options.get();
         return *this;
     }
 
--- include/obj/attributes.hpp
+++ include/obj/attributes.hpp
@@ -1,72 +1,104 @@
 #pragma once
 #include "../detail/common.hpp"
 #include "../detail/unwrap_handle.hpp"
+#include <cstdint>
 
 namespace ntw::obj {
 
-    /// \brief Class to build object attributes (besides the name)
-    class attributes {
-        OBJECT_ATTRIBUTES _attributes;
-
-    public:
-        /// \brief Initializes attributes with its length and other fields set to 0
-        NTW_INLINE attributes();
-
-        /// \brief Returns the internal OBJECT_ATTRIBUTES structure
-        OBJECT_ATTRIBUTES& get();
-
-        /// \brief Returns the internal OBJECT_ATTRIBUTES structure
-        const OBJECT_ATTRIBUTES& get() const;
-
-        /// \brief Sets the RootDirectory field of OBJECT_ATTRIBUTES
-        template<class Handle>
-        NTW_INLINE attributes& parent(const Handle& parent);
-
-        /// \brief Sets the SecurityDescriptor field of OBJECT_ATTRIBUTES
-        NTW_INLINE attributes& security_desc(void* descriptor);
-
-        /// \brief Sets the SecurityQualityOfService field of OBJECT_ATTRIBUTES
-        NTW_INLINE attributes& security_quality(void* quality_of_service);
-
+    /// \brief A type to build attribute options
+    /// \tparam Derived the derived class whose "options()" member function
+    ///         will be used to modify the state
+    template<class Derived>
+    struct basic_attribute_options {
         /// \brief Enables OBJ_INHERIT flag for the OBJECT_ATTRIBUTES
         /// \detail This handle can be inherited by children of the current process
-        NTW_INLINE attributes& inherit();
+        NTW_INLINE constexpr Derived& inherit();
 
         /// \brief Enables OBJ_PERMANENT flag for the OBJECT_ATTRIBUTES
         /// \detail Object will not be deleted when all open handles are closed.
         ///         ZwMakeTemporaryObject can be used to make object non-permanent
-        NTW_INLINE attributes& permanent();
+        NTW_INLINE constexpr Derived& permanent();
 
         /// \brief Enables OBJ_EXCLUSIVE flag for the OBJECT_ATTRIBUTES
         /// \detail No other process will be able to open a handle to this object
-        NTW_INLINE attributes& exclusive();
+        NTW_INLINE constexpr Derived& exclusive();
 
         /// \brief Enables OBJ_CASE_INSENSITIVE flag for the OBJECT_ATTRIBUTES
         /// \detail Case insensitive comparison will be used for the ObjectName member
-        NTW_INLINE attributes& case_insensitive();
+        NTW_INLINE constexpr Derived& case_insensitive();
 
         /// \brief Enables OBJ_OPENIF flag for the OBJECT_ATTRIBUTES
         /// \detail If object already exists STATUS_OBJECT_NAME_COLLISION will be returned
-        NTW_INLINE attributes& open();
+        NTW_INLINE constexpr Derived& open();
 
         /// \brief Enables OBJ_OPENLINK flag for the OBJECT_ATTRIBUTES
         /// \detail Opens symbolic link object itself instead of what it refers to
-        NTW_INLINE attributes& open_symlink();
+        NTW_INLINE constexpr Derived& open_symlink();
 
         /// \brief Enables OBJ_KERNEL_HANDLE flag for the OBJECT_ATTRIBUTES
         /// \detail Do not expose handle to current process if in kernel mode
-        NTW_INLINE attributes& kernel_handle();
+        NTW_INLINE constexpr Derived& kernel_handle();
 
         /// \brief Enables OBJ_FORCE_ACCESS_CHECK flag for the OBJECT_ATTRIBUTES
         /// \detail Enforce all access checks if in kernel mode
-        NTW_INLINE attributes& enforce_access_check();
+        NTW_INLINE constexpr Derived& enforce_access_check();
 
         /// \brief Enables OBJ_IGNORE_IMPERSONATED_DEVICEMAP flag
         /// \detail Ignore the impersonated device map
-        NTW_INLINE attributes& ignore_impersonated_devicemap();
+        NTW_INLINE constexpr Derived& ignore_impersonated_devicemap();
 
         /// \brief Enables OBJ_DONT_REPARSE flag
-        NTW_INLINE attributes& dont_reparse();
+        NTW_INLINE constexpr Derived& dont_reparse();
+    };
+
+
+    /// \brief A type to build attribute options separately
+    struct attribute_options : basic_attribute_options<attribute_options> {
+        unsigned long _options = 0;
+
+        NTW_INLINE constexpr unsigned long& options();
+
+        friend class basic_attribute_options<attribute_options>;
+
+    public:
+        /// \brief Construct options with null value
+        NTW_INLINE constexpr attribute_options() = default;
+
+        /// \brief Returns the built options value
+        NTW_INLINE constexpr unsigned long get() const;
+    };
+
+
+    /// \brief Class to build object attributes (besides the name)
+    class attributes : public basic_attribute_options<attributes> {
+        OBJECT_ATTRIBUTES _attributes;
+
+        NTW_INLINE constexpr unsigned long& options();
+
+        friend class basic_attribute_options<attributes>;
+
+    public:
+        /// \brief Initializes attributes with its length and other fields set to 0
+        NTW_INLINE constexpr attributes();
+
+        /// \brief Returns the internal OBJECT_ATTRIBUTES structure
+        NTW_INLINE constexpr OBJECT_ATTRIBUTES& get();
+
+        /// \brief Returns the internal OBJECT_ATTRIBUTES structure
+        NTW_INLINE constexpr const OBJECT_ATTRIBUTES& get() const;
+
+        /// \brief Sets the RootDirectory field of OBJECT_ATTRIBUTES
+        template<class Handle>
+        NTW_INLINE constexpr attributes& parent(const Handle& parent);
+
+        /// \brief Sets the SecurityDescriptor field of OBJECT_ATTRIBUTES
+        NTW_INLINE constexpr attributes& security_desc(void* descriptor);
+
+        /// \brief Sets the SecurityQualityOfService field of OBJECT_ATTRIBUTES
+        NTW_INLINE constexpr attributes& security_quality(void* quality_of_service);
+
+        /// \brief Sets the Attributes field of OBJECT_ATTRIBUTES
+        NTW_INLINE constexpr attributes& options(attribute_options options);
     };
 
 } // namespace ntw::obj
