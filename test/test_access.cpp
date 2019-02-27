e2c849b0f8b18b10e890eba4f9219240ff2d4934
JustasMasiulis <justas.masiulis@yahoo.com>
2019-02-27 16:50:18 +0200

    wrote tests for access

---
+++ test/test_access.cpp
@@ -0,0 +1,28 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include <obj/attributes.hpp>
+#include <access.hpp>
+
+struct mock_access : ntw::access_builder<mock_access> {};
+
+TEST_CASE("access correctly default constructed") { REQUIRE(mock_access{}.get() == 0); }
+
+TEST_CASE("access building works")
+{
+    auto access = mock_access{}
+                      .generic_read()
+                      .generic_write()
+                      .generic_execute()
+                      .generic_all()
+                      .system_security()
+                      .destroy()
+                      .read_security()
+                      .synchronize()
+                      .write_dac()
+                      .write_owner()
+                      .get();
+
+    REQUIRE(access == (GENERIC_READ | GENERIC_WRITE | GENERIC_EXECUTE | GENERIC_ALL |
+                       ACCESS_SYSTEM_SECURITY | DELETE | READ_CONTROL | SYNCHRONIZE |
+                       WRITE_DAC | WRITE_OWNER));
+}
