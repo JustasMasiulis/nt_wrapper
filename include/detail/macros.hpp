14f123fbbedc7497b54c78aefb8fc5aebba2fdc2
JustasMasiulis <justas.masiulis@yahoo.com>
2019-03-02 18:08:35 +0200

    adde a few new macros

--- include/detail/macros.hpp
+++ include/detail/macros.hpp
@@ -2,3 +2,11 @@
 
 #define NTW_INLINE __forceinline
 #define NTW_SYSCALL(fn) fn
+#define NTW_IMPORT_CALL(fn) fn
+
+#define NTW_RET_ON_ERR(expr)                         \
+    {                                                \
+        ::ntw::status _ntw_ret_on_err_status = expr; \
+        if(!_ntw_ret_on_err_status.success())        \
+            return_ntw_ret_on_err_status;            \
+    }
