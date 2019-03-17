fc33318558867e69db6392bd09116d5df1e8cee5
JustasMasiulis <justas.masiulis@yahoo.com>
2019-03-17 15:30:14 +0200

    adapted resource_wrapper to the changes

--- include/detail/resource_wrapper.hpp
+++ include/detail/resource_wrapper.hpp
@@ -4,74 +4,83 @@
 
 namespace ntw::detail {
 
+    template<class, class = void>
+    struct is_buffer : std::false_type {};
+
+    template<class T>
+    struct is_buffer<T,
+                     std::void_t<decltype(std::declval<T>().data()),
+                                 decltype(std::declval<T>().size_bytes())>>
+        : std::true_type {};
+
     namespace {
 
-        template<class, class = void>
-        struct is_buffer : std::false_type {};
-
-        template<class T>
-        struct is_buffer<T,
-                         std::void_t<decltype(std::declval<T>().data()),
-                                     decltype(std::declval<T>().size())>>
-            : std::true_type {};
-
-        template<class Alloc, bool IsBuffer = is_buffer<Alloc>::value>
-        struct resource_wrapper_base : Alloc {
+        template<class Alloc>
+        class resource_wrapper_alloc_base : Alloc {
             void*       _ptr  = nullptr;
             std::size_t _size = 0;
 
-            NTW_INLINE constexpr void* get() const { return _ptr; }
-            NTW_INLINE constexpr void* size() const { return _size; }
+        public:
+            using Alloc::deallocate;
+
+            NTW_INLINE constexpr void* data() const { return _ptr; }
+            NTW_INLINE constexpr void* size_bytes() const { return _size; }
 
             NTW_INLINE status allocate(std::size_t s)
             {
-                return static_cast<Alloc&>(*this).allocate(_ptr, s);
-            }
+                const auto status = static_cast<Alloc&>(*this).allocate(_ptr, s);
+                if(status.success())
+                    _size = s;
 
-            NTW_INLINE void deallocate() const
-            {
-                static_cast<Alloc&>(*this).deallocate(_ptr);
+                return status;
             }
         };
 
         template<class Buffer>
-        struct resource_wrapper_base<Buffer, true> : Buffer {
+        struct resource_wrapper_buffer_base : Buffer {
             using Buffer::data;
-            using Buffer::size;
+            using Buffer::size_bytes;
 
             NTW_INLINE status allocate(std::size_t s)
             {
-                return s > static_cast<Alloc&>(*this).size() ? STATUS_NO_MEMORY
-                                                             : STATUS_SUCCESS;
+                return s > size_bytes() ? STATUS_NO_MEMORY : STATUS_SUCCESS;
             }
 
-            NTW_INLINE constexpr static void deallocate() {}
+            NTW_INLINE constexpr static void deallocate(void*) {}
         };
+
+        template<class Resource>
+        using resource_wrapper_base =
+            std::conditional_t<is_buffer<Resource>::value,
+                               resource_wrapper_buffer_base<Resource>,
+                               resource_wrapper_alloc_base<Resource>>;
 
     } // namespace
 
 
     template<class Resource>
-    class resource_wrapper {
-        resource_wrapper_base<Resource> _base;
-
-        NTW_INLINE void _dealloc()
-        {
-            if(_base.get())
-                _base.deallocate();
-        }
+    class resource_wrapper : resource_wrapper_base<Resource> {
+        using base_type = resource_wrapper_base<Resource>;
 
     public:
-        NTW_INLINE ~resource_wrapper() { _dealloc(); }
+        using base_type::data;
+        using base_type::size_bytes;
+        using base_type::allocate;
+        using base_type::deallocate;
 
-        NTW_INLINE void* get() const { return _base.get(); }
-
-        NTW_INLINE std::size_t size() const { return _base.size(); }
-
-        NTW_INLINE status expand(std::size_t s)
+        NTW_INLINE ~resource_wrapper()
         {
-            _dealloc();
-            return _base.allocate(s);
+            if(const auto ptr = data(); ptr)
+                deallocate(ptr);
+        }
+
+        NTW_INLINE status realloc(std::size_t s)
+        {
+            const auto ptr    = data();
+            const auto status = allocate(s);
+            if(ptr && status.success())
+                deallocate(ptr);
+            return status;
         }
     };
 
