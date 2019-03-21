bcb67ddf91a4421d02837faa9367003f3e589ebf
JustasMasiulis <justas.masiulis@yahoo.com>
2019-03-21 19:12:15 +0200

    object tests

--- test/test_object.cpp
+++ test/test_object.cpp
@@ -5,7 +5,75 @@
 
 #pragma comment(lib, "ntdll.lib")
 
-TEST_CASE("object")
+TEST_CASE("object constructors and basic funcs")
 {
-    ntw::obj::unique_object obj{ NtCurrentProcess() };
+    SECTION("default constructed")
+    {
+        ntw::ob::unique_object obj;
+        REQUIRE(obj.get() == nullptr);
+    }
+
+    SECTION("handle constructed")
+    {
+        ntw::ob::unique_object obj{ NtCurrentProcess() };
+        REQUIRE(obj.get() == NtCurrentProcess());
+    }
+
+    SECTION("move constructed")
+    {
+        ntw::ob::unique_object obj{ NtCurrentProcess() };
+        ntw::ob::unique_object obj2{ std::move(obj) };
+
+        REQUIRE(obj2.get() == NtCurrentProcess());
+        REQUIRE(obj.get() == nullptr);
+    }
+
+    SECTION("move assigned")
+    {
+        ntw::ob::unique_object obj{ NtCurrentProcess() };
+        ntw::ob::unique_object obj2{ nullptr };
+        obj2 = std::move(obj);
+
+        REQUIRE(obj2.get() == NtCurrentProcess());
+        REQUIRE(obj.get() == nullptr);
+    }
+}
+
+TEST_CASE("reset")
+{
+    SECTION("non null")
+    {
+        ntw::ob::unique_object obj{ NtCurrentProcess() };
+        obj.reset(nullptr);
+        REQUIRE(obj.get() == nullptr);
+    }
+
+    SECTION("null")
+    {
+        ntw::ob::unique_object obj;
+        obj.reset(NtCurrentProcess());
+        REQUIRE(obj.get() == NtCurrentProcess());
+    }
+}
+
+TEST_CASE("release")
+{
+    ntw::ob::unique_object obj{ NtCurrentProcess() };
+    REQUIRE(obj.release() == NtCurrentProcess());
+    REQUIRE(obj.get() == nullptr);
+}
+
+TEST_CASE("operator bool")
+{
+    SECTION("non null")
+    {
+        ntw::ob::unique_object obj{ NtCurrentProcess() };
+        REQUIRE(!!obj);
+    }
+
+    SECTION("null")
+    {
+        ntw::ob::unique_object obj;
+        REQUIRE(!obj);
+    }
 }
