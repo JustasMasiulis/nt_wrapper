bc69acab6490b8995b9263d774256eab377f3175
JustasMasiulis <justas.masiulis@yahoo.com>
2019-12-15 00:36:06 +0200

    new tests

---
+++ test/test_loaded_modules.cpp
@@ -0,0 +1,17 @@
+#include <ntw/sys/modules.hpp>
+#define CATCH_CONFIG_MAIN
+#define WIN32_NO_STATUS
+#include <catch2/catch.hpp>
+
+#pragma comment(lib, "ntdll.lib")
+
+TEST_CASE("loaded_modules")
+{
+    alignas(16) std::uint8_t arr[0x1000 * 32] = { 0 };
+    const auto               r = ntw::sys::acquire_loaded_modules(gsl::make_span(arr));
+    INFO(std::hex << r.get());
+    REQUIRE(r.success());
+
+    for(const auto& lm : *r)
+        CHECK(lm.image_base != 0);
+}
