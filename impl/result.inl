fd144ca98b4f0d9f038b8899ba5086a224331902
JustasMasiulis <justas.masiulis@yahoo.com>
2019-12-24 14:38:12 +0200

    various result type improvements and fixes

--- impl/result.inl
+++ impl/result.inl
@@ -4,61 +4,72 @@
 namespace ntw {
 
     template<class T>
-    inline constexpr status& result<T>::status() noexcept
+    NTW_INLINE constexpr status& result<T>::status() noexcept
     {
         return *this;
     }
 
     template<class T>
-    inline constexpr const status& result<T>::status() const noexcept
+    NTW_INLINE constexpr const status& result<T>::status() const noexcept
     {
         return *this;
     }
 
     template<class T>
-    constexpr T& result<T>::operator*() noexcept
+    NTW_INLINE constexpr T& result<T>::operator*() noexcept
     {
         return _value;
     }
 
     template<class T>
-    constexpr const T& result<T>::operator*() const noexcept
+    NTW_INLINE constexpr const T& result<T>::operator*() const noexcept
     {
         return _value;
     }
 
     template<class T>
-    inline constexpr const T* ntw::result<T>::operator->() const noexcept
+    NTW_INLINE constexpr const T* ntw::result<T>::operator->() const noexcept
     {
         return &_value;
     }
 
     template<class T>
-    inline constexpr T* ntw::result<T>::operator->() noexcept
+    NTW_INLINE constexpr T* ntw::result<T>::operator->() noexcept
     {
         return &_value;
     }
 
     template<class T>
-    inline constexpr status& ntw::result_ref<T>::status() noexcept
+    template<class Fn>
+    NTW_INLINE constexpr auto ntw::result<T>::then(Fn fn)
+        -> ntw::result<decltype(fn(**this))>
+    {
+        if(*this)
+            return { status(), fn(**this) };
+        return { status() };
+    }
+
+    template<class T>
+    NTW_INLINE constexpr status& ntw::result_ref<T>::status() noexcept
     {
         return *this;
     }
 
     template<class T>
-    inline constexpr const status& ntw::result_ref<T>::status() const noexcept
+    NTW_INLINE constexpr const status& ntw::result_ref<T>::status() const noexcept
     {
         return *this;
     }
 
     template<class T>
-    constexpr std::add_lvalue_reference_t<T> ntw::result_ref<T>::operator*() const
+    NTW_INLINE constexpr std::add_lvalue_reference_t<T>
+        ntw::result_ref<T>::operator*() const
     {
         return *_value;
     }
 
     template<class T>
-    inline constexpr T* ntw::result_ref<T>::operator->() const noexcept
+    NTW_INLINE constexpr T* ntw::result_ref<T>::operator->() const noexcept
     {
         return _value;
     }
--- include/ntw/result.hpp
+++ include/ntw/result.hpp
@@ -7,44 +7,54 @@ namespace ntw {
     template<class T>
     class result final : public ntw::status {
         using base_type = ntw::status;
-        static_assert(std::is_trivially_copyable<T>::value,
-                      "ntw::result expect a trivially copyable type");
         T _value;
 
     public:
-        constexpr result() = default;
+        NTW_INLINE constexpr result() = default;
 
-        constexpr result(status s, T&& v) : base_type(s), _value(std::move(v)) {}
-        constexpr result(status s, const T& v) : base_type(s), _value(v) {}
+        NTW_INLINE constexpr result(const result&)  = default;
+        NTW_INLINE constexpr result(result&& other) = default;
 
-        constexpr status&            status() noexcept;
-        constexpr const ntw::status& status() const noexcept;
+        NTW_INLINE constexpr result& operator=(const result&) = default;
+        NTW_INLINE constexpr result& operator=(result&& other) = default;
 
-        constexpr const T& operator*() const noexcept;
-        constexpr T&       operator*() noexcept;
+        NTW_INLINE constexpr result(status s) : base_type(s) {}
+        NTW_INLINE constexpr result(status s, T&& v) : base_type(s), _value(std::move(v))
+        {}
+        NTW_INLINE constexpr result(status s, const T& v) : base_type(s), _value(v) {}
 
-        constexpr const T* operator->() const noexcept;
-        constexpr T*       operator->() noexcept;
+        NTW_INLINE constexpr status&            status() noexcept;
+        NTW_INLINE constexpr const ntw::status& status() const noexcept;
+
+        NTW_INLINE constexpr const T& operator*() const noexcept;
+        NTW_INLINE constexpr T&       operator*() noexcept;
+
+        NTW_INLINE constexpr const T* operator->() const noexcept;
+        NTW_INLINE constexpr T*       operator->() noexcept;
+
+        NTW_INLINE constexpr operator bool() const noexcept { return success(); }
+
+        template<class Fn>
+        NTW_INLINE constexpr auto then(Fn fn)->ntw::result<decltype(fn(**this))>;
     };
 
     template<class T>
     class result_ref final : public status {
         using base_type = ntw::status;
-        static_assert(std::is_trivial_v<T>, "ntw::result expect a trivial type");
-        T* _value = nullptr;
+        T* _value       = nullptr;
 
     public:
-        constexpr result_ref() = default;
+        NTW_INLINE constexpr result_ref() = default;
 
         template<class U>
-        constexpr result_ref(status s, T* v) : base_type(s), _value(v)
+        NTW_INLINE constexpr result_ref(status s, T* v) : base_type(s), _value(v)
         {}
 
-        constexpr status&            status() noexcept;
-        constexpr const ntw::status& status() const noexcept;
+        NTW_INLINE constexpr status&            status() noexcept;
+        NTW_INLINE constexpr const ntw::status& status() const noexcept;
 
-        constexpr std::add_lvalue_reference_t<T> operator*() const;
-        constexpr T*                             operator->() const noexcept;
+        NTW_INLINE constexpr std::add_lvalue_reference_t<T> operator*() const;
+        NTW_INLINE constexpr T*                             operator->() const noexcept;
     };
 
 } // namespace ntw
