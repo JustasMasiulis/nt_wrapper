a42cc3d49846eaa57c42061b16226d3d178d1c7c
JustasMasiulis <justas.masiulis@yahoo.com>
2020-05-26 23:02:02 +0300

    added job tests

---
+++ test/test_job.cpp
@@ -0,0 +1,32 @@
+#include <ntw/ob/job.hpp>
+#define CATCH_CONFIG_MAIN
+#define WIN32_NO_STATUS
+#include <catch2/catch.hpp>
+
+#pragma comment(lib, "ntdll.lib")
+
+TEST_CASE("job")
+{
+    using namespace ntw::ob;
+
+    auto res = job::create(job_access{}.all());
+    CHECK(res);
+}
+
+TEST_CASE("job access")
+{
+    using namespace ntw::ob;
+
+    CHECK(job_access{}.all().get() == JOB_OBJECT_ALL_ACCESS);
+    CHECK((job_access{}
+               .assign_process()
+               .set_attributes()
+               .query()
+               .terminate()
+               .set_security_attr()
+               .impersonate()
+               .synchronize()
+               .get() |
+           STANDARD_RIGHTS_REQUIRED)
+          == JOB_OBJECT_ALL_ACCESS);
+}
