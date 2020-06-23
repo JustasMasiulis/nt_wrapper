a7d5073ab5a0c1ea32e38c3870e4eede9fc2257d
unknown <42058587+aleks-s@users.noreply.github.com>
2020-06-24 09:07:53 +1000

    removed unused macros

--- include/ntw/detail/macros.hpp
+++ include/ntw/detail/macros.hpp
@@ -5,10 +5,3 @@
 #define NTW_SYSCALL(fn) fn
 #endif
 #define NTW_IMPORT_CALL(fn) fn
-
-#define NTW_RET_ON_ERR(expr)                         \
-    {                                                \
-        ::ntw::status _ntw_ret_on_err_status = expr; \
-        if(!_ntw_ret_on_err_status.success())        \
-            return _ntw_ret_on_err_status;           \
-    }
